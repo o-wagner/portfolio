90dea8dc3e2d10fbb20f71438741a8ddefb07d67
WAGNER AUGUSTO DE OLIVEIRA <0041938@IFMG.EDU.BR>
2023-05-04 16:12:41 -0300

    alter

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,7 +3,7 @@ import quiz from '../img/projects/quiz.png';
 import buscacep from '../img/projects/buscacep.png';
 import brasileirao from '../img/projects/brasileirao.png'
 import qrcode from '../img/projects/qrcode.png';
-import myservices from '../img/projects/myservices.jpg'; 
+import myservices from '../img/projects/myservices.jpg';
 const projects = [
   {
     id: 1,
@@ -12,39 +12,40 @@ const projects = [
     img: quiz,
     link: 'https://github.com/o-wagner/Quiz'
   },
-  { 
-    id: 2, 
-    name: 'Gerador de QR CODE', 
-    description: 'Página Web com gerador de QR CODE básico, projeto desenvolvido também para um trabalho da faculdade utilizando REACT.', 
+  {
+    id: 2,
+    name: 'Gerador de QR CODE',
+    description: 'Página Web com gerador de QR CODE básico, projeto desenvolvido também para um trabalho da faculdade utilizando REACT.',
     img: qrcode,
-    link: 'https://github.com/o-wagner/Gerador-de-QRCode' 
+    link: 'https://github.com/o-wagner/Gerador-de-QRCode'
   },
-  { 
+  {
     id: 3,
-     name: 'BuscaCEP', 
-     description: 'Página Web com um formulário que completa os campos de endereço após preencher o campo CEP.' +
-     ' Projeto desenvolvido em REACT.', 
-     img: buscacep ,
-     link: 'https://github.com/o-wagner/buscaCEP'
+    name: 'BuscaCEP',
+    description: 'Página Web com um formulário que completa os campos de endereço após preencher o campo CEP.' +
+      ' Projeto desenvolvido em REACT.',
+    img: buscacep,
+    link: 'https://github.com/o-wagner/buscaCEP'
   },
   {
     id: 4,
+    name: 'MyServices',
+    description: 'O My Services é um projeto desenvolvido em React Native, usando Expo, foi desenvolvido para o trabalho final'
+      + ' da disciplina de desenvolvimento mobile, com o intuito de fazer um protótipo para o meu projeto de TCC',
+    img: myservices,
+    link: 'https://github.com/o-wagner/my-services'
+  },
+  {
+    id: 5,
     name: 'Tabela do Brasileirão',
-    description: 'Tabela de classificação Brasileirão Serie A.' 
-    + ' Projeto desenvolvido com React utilizando API de futebol de times brasileiros.',  
+    description: 'Tabela de classificação Brasileirão Serie A.'
+      + ' Projeto desenvolvido com React utilizando API de futebol de times brasileiros.',
     img: brasileirao,
     link: 'https://github.com/o-wagner/brasileirao'
-  },
-  { 
-    id: 5,
-     name: 'MyServices', 
-     description: 'O My Services é um projeto desenvolvido em React Native, usando Expo, foi desenvolvido para o trabalho final'
-     +' da disciplina de desenvolvimento mobile, com o intuito de fazer um protótipo para o meu projeto de TCC', 
-     img: myservices,
-     link: 'https://github.com/o-wagner/my-services'
-  },
- 
+  }
   
+
+
 
 ]
 
@@ -56,7 +57,9 @@ const ProjectsContainer = () => {
       <div className="projects-grid">
         {projects.map((proj) => (
           <div className="projects-card" id={proj.id} key={proj.id}>
-            <h4>{proj.name}</h4>
+            <div className="proj-nome">
+              <h4>{proj.name}</h4>
+            </div>
             <div className="imagem">
               <img src={proj.img} />
             </div>
